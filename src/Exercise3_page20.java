8e04d0ce92fcd6b48b8b98d22a8cbfcbd08b5f94
RyhorKisly <RyhorKisly@gmail.com>
2022-09-30 18:48:41 +0300

    Initial commit

---
+++ src/Exercise3_page20.java
@@ -0,0 +1,14 @@
+public class Exercise3_page20 {
+    public static void main(String[] args) {
+
+        byte b = 0x55;
+        short s = 0x55ff;
+        int i = 1_000_000;
+        long I = 0xffffffffL;
+        char c ='a';
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+        System.out.println(b + "\n" + s + "\n" + i + "\n" + I + "\n" + c + "\n" + f + "\n" + d + "\n" + bool);
+    }
+}
---
+++ src/Exercise5_page20.java
@@ -0,0 +1,12 @@
+public class Exercise5_page20 {
+    public static void main(String[] args) {
+        int sec = 1295435;
+        int s = sec % 60;            //35
+        int min = (sec -s) / 60;     //21590
+        int m = min % 60;            //50
+        int hours = (min - m) / 60;  //359
+        int h = hours % 24;          //23
+        int d = (hours - h) / 24;  //14
+        System.out.println(d + " дней " + h + " часа " + m + " минут " + s + " секунд ");
+    }
+}
