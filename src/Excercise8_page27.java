f377150f6170584f931c6c89aad74a8f18d4edb8
RyhorKisly <RyhorKisly@gmail.com>
2022-09-30 19:16:38 +0300

    excercise8

---
+++ src/Excercise8_page27.java
@@ -0,0 +1,14 @@
+public class Excercise8_page27 {
+    public static void main(String[] args) {
+
+        int x = -269;
+        int y = x % 10;
+        if (y == 1) {
+            System.out.println("x = " + x + " рубль");
+        } else if (y >= 2 && y <= 4) {
+            System.out.println("x = " + x + " рубля");
+        } else {
+            System.out.println("x = " + x + " рублей");
+        }
+    }
+}
