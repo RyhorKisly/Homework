26e06774321946c0162627cf1e29b4e9d98f3df6
RyhorKisly <RyhorKisly@gmail.com>
2022-10-15 18:21:56 +0300

    ...

---
+++ src/Homework_from_Roman/hw2/exercise02_and_exercise10/Box.java
@@ -0,0 +1,104 @@
+// Задача 2:
+// Задача с классом Box.
+// Необходимо добавить в класс возможность определить какого типа коробка у нас создана.
+// 1) Куб (Все стороны равны).
+// 2) Обычная коробка.
+// 3) Конверт (Есть только длинна и ширина).
+//
+//Задача 10:
+//Используя класс Box из задания 2, необходимо
+//добавить в него конструктор который принимает строку вида:
+//
+//Box[1,2,3] или Box[2] или Box[1,2]
+//
+//И как результат создает соответствующий обьект.
+
+        package Homework_from_Roman.hw2.exercise02_and_exercise10;
+
+import java.util.Random;
+
+public class Box {
+    private String typeOfBox;
+    private int length;
+    private int width;
+    private int height;
+    private static final Random RANDOM = new Random();
+
+    public Box(String typeOfBox, int length, int width, int height) {
+        this.typeOfBox = typeOfBox;
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+//    public Box(String box) {                                         // нужно создать объект в конструкторе?
+//        int length = RANDOM.nextInt(10);                             // или просто конструктор,
+//        this.length = length;                                        // а в Main создать объект???
+//        int width = RANDOM.nextInt(10);
+//        this.width = width;
+//        int height = RANDOM.nextInt(10);
+//        this.height = height;
+//        if (box.equals("Box[1,2,3]") && (length * width * height != 0)) {
+//            Box box2  = new Box ("BoxType", length, width, height);
+//            box2.typeBox();
+//        } else if (box.equals("Box[1,2]") && (length * width != 0)) {
+//            Box box2  = new Box ("BoxType", length, width, 0);
+//            box2.typeBox();
+//        } else if (box.equals("Box[2]") && length != 0) {
+//            Box box2  = new Box ("BoxType", length, length, length);
+//            box2.typeBox();
+//        }
+//
+//    }
+
+    public Box(String box) {
+        int length = RANDOM.nextInt(10);
+        int width = RANDOM.nextInt(10);
+        int height = RANDOM.nextInt(10);
+        if (box.equals("Box[1,2,3]") && (length * width * height != 0)) {
+            Box box2  = new Box ("BoxType", length, width, height);
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        } else if (box.equals("Box[1,2]") && (length * width != 0)) {
+            this.length = length;
+            this.width = width;
+            this.height = 0;
+        } else if (box.equals("Box[2]") && length != 0) {
+            this.length = length;
+            this.width = length;
+            this.height = length;
+        }
+    }
+
+    public String getTypeOfBox() {
+        return typeOfBox;
+    }
+
+    public void print() {
+        System.out.println(
+                "Тип коробки: " + typeOfBox +
+                        " Длина: " + length +
+                        "; Ширина: " + width +
+                        "; Высота: " + height + "."
+        );
+    }
+
+    public void printTypeBox() {
+        if (length * width * height < 0) {
+            typeOfBox = "Неизвестный тип коробки:";
+        }else if (length == width && width == height && (length != 0)) {
+            typeOfBox = "Это Куб:";
+        } else if (height == 0 && (length * width != 0)) {
+            typeOfBox = "Это конверт:";
+        } else if ((length * width * height != 0)) {
+            typeOfBox = "Это обычная коробка:";
+        }  else {
+            typeOfBox = "Неизвестный тип коробки:";
+        }
+        print();
+    }
+
+
+
+}
---
+++ src/Homework_from_Roman/hw2/exercise02_and_exercise10/Main.java
@@ -0,0 +1,16 @@
+package Homework_from_Roman.hw2.exercise02_and_exercise10;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Box box = new Box("BoxType:", 6, -7,3);
+        box.printTypeBox();
+        System.out.println();
+        Box box2 = new Box("Box[1,2]");
+        box2.printTypeBox();
+
+    }
+
+
+}
