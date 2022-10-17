29866c557f43db5742044fe1fb73017c7e54bbc9
RyhorKisly <RyhorKisly@gmail.com>
2022-10-17 16:18:07 +0300

    ...

---
+++ src/Homework_from_Roman/hw2/exercise03/Number.java
@@ -0,0 +1,40 @@
+//Задача 3:
+//Необходимо создать класс который имеет метод чтобы накапливать 5 последних переданных
+//в него цифровых значений. Так же необходимо создать метод который выводит
+//среднее из накопленных значений.
+
+package Homework_from_Roman.hw2.exercise03;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Number {
+
+    private int i = 0;
+    private static final int[] LENGTHNUMBER = new int[50];
+    int sum = 0;
+    double average = 0;
+
+    public int consume(int count) {
+        if (i < 5) {
+            LENGTHNUMBER[i++] = count;
+            sum += count;
+        } else {
+            LENGTHNUMBER[i] = count;
+            sum += count;
+            sum -= LENGTHNUMBER[i++ - 5];
+        }
+        System.out.println("sum: " + sum);
+        return sum;
+    }
+
+    public void avg() {
+        if (i < 5) {
+            average = (double)sum / i;
+        } else {
+            average = (double)sum / 5;
+        }
+        System.out.println(average);
+    }
+
+}
