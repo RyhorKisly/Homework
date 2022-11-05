8727a9ed78abb939e23517425b87f15422636353
RyhorKisly <RyhorKisly@gmail.com>
2022-11-05 15:15:56 +0300

    ...

--- src/Homework_from_Roman/extra/generic/exerciseTwo/Pair.java
+++ src/Homework_from_Roman/extra/generic/exerciseTwo/Pair.java
@@ -1,4 +1,39 @@
+//        Реализовать класс Pair, который будет содержать 2 значения любого типа.
+
 package Homework_from_Roman.extra.generic.exerciseTwo;
 
-public class Pair {
+public class Pair<T, S> {
+
+    private T first;
+    private S second;
+
+    public Pair(T first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+//        * first() - возвращает 1ый элемент
+    public T getFirst() {
+        return first;
+    }
+
+//        * last() - возвращает 2ой элемент
+    public S getSecond() {
+        return second;
+    }
+
+//        * replaceFirst() - заменяет 1ый элемент на новый
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+//        * replaceLast() - заменяет 2ой элемент на новый
+    public void setSecond(S second) {
+        this.second = second;
+    }
+
+//        * swap() - меняет элементы местами                           ???
+    public void swapFirstAndSecond() {
+
+    }
 }
