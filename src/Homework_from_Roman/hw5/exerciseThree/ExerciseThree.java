91b15f98a704aad6845ed73a3de3abe80ba97eb2
RyhorKisly <RyhorKisly@gmail.com>
2022-11-07 00:56:15 +0300

    ...

---
+++ src/Homework_from_Roman/hw5/exerciseThree/ExerciseThree.java
@@ -0,0 +1,55 @@
+//        3
+//        Необходимо реализовать программу, которая будет считывать
+//        числа, введенные пользователем, пока пользователь не введет
+//        "stop" слово. по команде "status" необходимо вывести введенные
+//        цифры в отсортированном виде.
+
+package Homework_from_Roman.hw5.exerciseThree;
+
+import java.util.*;
+
+public class ExerciseThree {
+
+    private Scanner in = new Scanner(System.in);
+    private List<Integer> arrayList = new ArrayList<>();
+    private int number = 0;
+
+    public void getSortedNumbers() {
+        try {
+            while (true) {
+                if (in.hasNextInt()) {
+                    number = in.nextInt();
+                    arrayList.add(number);
+                    System.out.println(number);
+                } else {
+                    if (in.next().equals("ESC")) {
+                        break;
+                    }
+                    System.out.println("Введите целое число или stop слово: \"ESC\"");
+                }
+            }
+            while (true) {
+                if (in.nextLine().equals("status")) {
+                    Collections.sort(arrayList);
+                    break;
+                } else {
+                    System.out.println("Введите слово \"status\"");
+                }
+            }
+        } catch (NoSuchElementException | IllegalStateException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                in.close();
+            } catch (IllegalStateException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        for(Integer element : arrayList) {
+            System.out.print(element + " ");
+        }
+    }
+
+}
+
