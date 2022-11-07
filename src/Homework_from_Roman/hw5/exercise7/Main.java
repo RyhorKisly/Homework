e4b49e4771f9bc1e1f8e3ebf2e7a6d666e886782
RyhorKisly <RyhorKisly@gmail.com>
2022-11-07 22:20:44 +0300

    ...

--- src/Homework_from_Roman/hw5/exercise7/Main.java
+++ src/Homework_from_Roman/hw5/exercise7/Main.java
@@ -14,21 +14,6 @@ import java.util.NoSuchElementException;
 public class Main {
     public static void main(String[] args) {
 
-        try(BufferedWriter bw = new BufferedWriter(
-                    new FileWriter("C:\\Users\\Workstation\\" +
-                            "IdeaProjects\\Homework\\src\\Homework_from_Roman\\" +
-                            "hw5\\exercise7\\OUTPUT")))
-        {
-            List<String> lines = Files.readAllLines(Paths.get(
-                    "C:\\Users\\Workstation\\IdeaProjects\\Homework\\src\\Homework_from_Roman\\hw5\\exercise7\\INPUT"));
-            ListIterator li = lines.listIterator(lines.size());
-            while(li.hasPrevious()) {
-                bw.write((String)li.previous() + "\n");
-            }
-        }
-        catch(IOException | NoSuchElementException ex){
-            System.out.println(ex.getMessage());
-        }
-    }
+
 
 }
---
+++ src/Homework_from_Roman/hw5/exercise8/Main.java
@@ -0,0 +1,11 @@
+//        8
+//        Имеется два файла (INPUT_1 и INPUT_2). В третий (OUTPUT) необходимо
+//        записать только те строки, которые есть и в первом, и во втором.
+
+package Homework_from_Roman.hw5.exercise8;
+
+public class Main {
+
+
+
+}
