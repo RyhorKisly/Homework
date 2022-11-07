c3122c45c44a0e132b5e55d33ba96f192ddcaf8b
RyhorKisly <RyhorKisly@gmail.com>
2022-11-07 21:44:57 +0300

    Ещё не готово

--- src/Homework_from_Roman/hw5/exercise6Later/Main.java
+++ src/Homework_from_Roman/hw5/exercise6Later/Main.java
@@ -13,6 +13,7 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
 
+
 //        List<String> arrayLines = new ArrayList<>();
 //
 //                try (BufferedReader reader = new BufferedReader(
