f9f573f8f87dd14655d67818dcfb344e89da4ba7
RyhorKisly <RyhorKisly@gmail.com>
2022-11-26 12:36:47 +0300

    ...

--- src/Homework_from_Roman/hw4/factories/Factory.java
+++ src/Homework_from_Roman/hw4/factories/Factory.java
@@ -27,7 +27,8 @@ public abstract class Factory {
         this.engineVolume = engineVolume;
     }
 
-    protected abstract Car createCar(Model model, EngineVolume engineVolume, Colour colour, WheelSize wheelSize, Set<Option> option, SpecialOption specialOption);
+    protected abstract Car createCar(Model model, EngineVolume engineVolume, Colour colour, WheelSize wheelSize,
+                                     Set<Option> option, SpecialOption specialOption);
 
     public String getConfigurations() {
         return format(
