411273e871c6621b8e709f40143cd671527700cc
RyhorKisly <RyhorKisly@gmail.com>
2022-12-03 15:05:59 +0300

    ...

---
+++ src/Homework_from_Roman/hw6/MainNew.java
@@ -0,0 +1,29 @@
+package Homework_from_Roman.hw6;
+
+import java.util.Random;
+
+public class MainNew{
+    private static boolean aBoolean = true;
+
+    public static void main(String[] args) {
+
+        if(!aBoolean) {
+            MethodOne();
+        }
+
+    }
+
+    public static void MethodOne() {
+        System.out.println("Метод один");
+    }
+
+    public static void methodTwo() {
+        System.out.println("Метод два");
+        aBoolean = false;
+    }
+
+
+
+    }
+
+
