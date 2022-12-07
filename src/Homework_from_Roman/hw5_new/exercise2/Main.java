17f7d1e1d778a817c368852ae3710cf3bb63a4cb
RyhorKisly <RyhorKisly@gmail.com>
2022-12-07 23:22:24 +0300

    ...

--- src/Homework_from_Roman/hw5_new/exercise2/Main.java
+++ src/Homework_from_Roman/hw5_new/exercise2/Main.java
@@ -12,10 +12,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
 
